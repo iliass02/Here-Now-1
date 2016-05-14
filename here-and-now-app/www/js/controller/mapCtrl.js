fc50a9f5816ab7f94d4e415d9413aa75e046a558
iliass <iliass.marchoud@gmail.com>
2016-05-14 21:47:18 +0200

    display all interests

--- here-and-now-app/www/js/controller/mapCtrl.js
+++ here-and-now-app/www/js/controller/mapCtrl.js
@@ -2,12 +2,13 @@ app
 
   .controller('MapCtrl', function($scope, $state, $cordovaGeolocation, $ionicLoading, $stateParams, $http, NgMap) {
     var options = {timeout: 10000, enableHighAccuracy: true};
+    var GoogleKey = "AIzaSyAksXWsv6qT5z_DJk-kWW5wmDXs1TG_BP8";
 
     $ionicLoading.show({
       template: '<ion-spinner icon="android"></ion-spinner>'
     });
 
-    $scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyAksXWsv6qT5z_DJk-kWW5wmDXs1TG_BP8";
+    $scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key="+GoogleKey;
 
 
         $cordovaGeolocation.getCurrentPosition(options).then(function(position) {
@@ -18,21 +19,26 @@ app
           $http.get(path_url + '/api/v1/interests/user/' + $stateParams.user_id)
             .success(function (data) {
               var interests = data.data;
-
-
-
-
+              var allInterest;
               var params = {
                 location: position.coords.latitude+', '+position.coords.longitude,
                 radius: 500,
-                types: 'university|food',
+                types: allInterest,
                 key: 'AIzaSyAksXWsv6qT5z_DJk-kWW5wmDXs1TG_BP8'
-              }
+              };
 
+              for (i = 0; i < interests.length; i ++) {
+                if (i == 0) {
+                  allInterest = interests[i].name;
+                } else {
+                  allInterest = allInterest+'|'+interests[i].name;
+                }
+              }
 
 
               $http.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json", {params: params})
                 .success(function (data) {
+                  console.log(allInterest);
                   $scope.interests = data.results;
                 })
                 .error(function (err) {
@@ -40,12 +46,12 @@ app
                 })
                 .finally(function () {
                   $ionicLoading.hide();
-                })
+                });
 
             })
             .error(function (err) {
               console.log(err);
-            })
+            });
         });
 
   })
