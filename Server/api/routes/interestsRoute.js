5f06d921033b9827c7199367d3c02f30d2d31f04
iliass <iliass.marchoud@gmail.com>
2016-05-17 17:31:25 +0200

    add get and post favorites

--- Server/api/routes/interestsRoute.js
+++ Server/api/routes/interestsRoute.js
@@ -5,6 +5,7 @@ module.exports = function(router, connection) {
     //model
     var interest = require('../../models/interest')(connection, Sequelize);
     var userInterest = require('../../models/users_interest')(connection, Sequelize);
+    var favorites = require('../../models/favorites')(connection, Sequelize);
 
     router.route('/interests')
         .get(function (req, res) {
@@ -48,5 +49,59 @@ module.exports = function(router, connection) {
             }
         });
 
+    router.route('/users/:userId/interests/favorites')
+        .post(function (req, res) {
+
+            var userId = req.params.userId;
+            var address = req.body.address;
+            var name = req.body.name;
+            var latitude = req.body.latitude;
+            var longitude = req.body.longitude;
+            
+
+            if (!address || !name || !latitude || !longitude) {
+                res.status(500).send({
+                    success: false,
+                    error: "adresse, name, latitude, longitude parameters are required !"
+                });
+            } else {
+                favorites.create({
+                    address: address,
+                    name: name,
+                    latitude: latitude,
+                    longitude: longitude,
+                    user_id: userId
+                }).then(function (favorite) {
+                    res.status(200).send({
+                        success: true,
+                        data: favorite
+                    });
+                }).error(function (err) {
+                    res.status(500).send({
+                        success: true,
+                        error: err
+                    });
+                });
+            }
+
+        })
+
+        .get(function (req, res) {
+
+            var userId = req.params.userId;
+
+            favorites.findAll().then(function(favorites) {
+               res.status(200).send({
+                   success: true,
+                   data: favorites
+               });
+            }).error(function (err) {
+                res.status(500).send({
+                    success: true,
+                    error: err
+                });
+            });
+
+        })
 
 };
