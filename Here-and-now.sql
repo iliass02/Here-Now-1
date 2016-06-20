da408febd408a8c7fdef7f4737147f694b296b84
iliass <iliass.marchoud@gmail.com>
2016-06-20 17:43:37 +0200

    update database

--- Here-and-now.sql
+++ Here-and-now.sql
@@ -7,7 +7,7 @@
 #
 # Hôte: 127.0.0.1 (MySQL 5.5.42)
 # Base de données: Here-and-now
-# Temps de génération: 2016-05-18 12:35:34 +0000
+# Temps de génération: 2016-06-20 15:43:01 +0000
 # ************************************************************
 
 
@@ -42,14 +42,13 @@ LOCK TABLES `favorites` WRITE;
 
 INSERT INTO `favorites` (`id`, `user_id`, `name`, `address`, `latitude`, `longitude`)
 VALUES
-	(1,1,'mon adresse','2 square chasles','2','3'),
 	(2,1,'mon adresse','2 square chasles','2','3'),
-	(6,7,'Guerin','7 Place Colbert, Meaux','49','3'),
-	(7,7,'Guerin','7 Place Colbert, Meaux','48.9496','2.90501'),
-	(8,7,'Guerin','7 Place Colbert, Meaux','48.9495783','2.905014'),
-	(9,7,'mon adresse','2 square chasles','2','3'),
-	(10,7,'Conseil Général de Seine et Marne','4 Square Pierre et Marie Curie, Meaux','48.9492596','2.902442999999999'),
-	(11,7,'DataWolf','2 Square Clairaut, Meaux','48.94871970000001','2.903391');
+	(13,1,'Médiathèque Chenonceau','10 Rue Winston Churchill, Meaux','48.9501838','2.9042874'),
+	(14,1,'Guerin','7 Place Colbert, Meaux','48.9495783','2.905014'),
+	(15,1,'Triquenot Maxime','4 Square Chasles, Meaux','48.9492493','2.9039116'),
+	(18,9,'I.N.R.A','65 Boulevard de Brandebourg, Ivry-sur-Seine','48.81418679999999','2.393429'),
+	(19,1,'Score Service','63 Boulevard de Brandebourg, Ivry-sur-Seine','48.81447530000001','2.393758399999999'),
+	(31,1,'L\'espoir Sarl','22 Rue Winston Churchill, Meaux','48.95021439999999','2.9058981');
 
 /*!40000 ALTER TABLE `favorites` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -84,6 +83,36 @@ VALUES
 UNLOCK TABLES;
 
 
+# Affichage de la table news_feed
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `news_feed`;
+
+CREATE TABLE `news_feed` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `content` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Affichage de la table opinions
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `opinions`;
+
+CREATE TABLE `opinions` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `interest_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `content` text,
+  `date_post` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
 # Affichage de la table users
 # ------------------------------------------------------------
 
@@ -103,7 +132,11 @@ LOCK TABLES `users` WRITE;
 INSERT INTO `users` (`id`, `login`, `email`, `password`)
 VALUES
 	(1,'login-user','email-user','$2a$10$hgSaY.DC.LTP1daE0q.SB.sJJFW6SHGCJ6GdJkc7X1s5zkW3xZkfe'),
-	(7,'Marchoud_Iliass','iliass.marchoud@gmail.com','$2a$10$3yHbR46JeY5UELDlW3jtu.4OVy/F4aCHZnVF29msOeh9ttcJ0ykF.');
+	(7,'Marchoud_Iliass','iliass.marchoud@gmail.com','$2a$10$3yHbR46JeY5UELDlW3jtu.4OVy/F4aCHZnVF29msOeh9ttcJ0ykF.'),
+	(8,'Sadam_Houssam','shadow_1996@hotmail.fr','$2a$10$5qCBG/mMlte0SOUgjTVt3.cr/sw07B5sUAS7JqAa3AabWl6qKPWo2'),
+	(9,'Fin_Pomponette','pomponette93420@gmail.com','$2a$10$9xYe2ynIi1ykcWEFhD/heeNKX.MS5CnQluwua7ELOu8YttYmaXm5q'),
+	(11,'marchoud_fatima','fatima.marchoud.maamri@gmail.com','$2a$10$0c3cqWQvcs6y.yyVvwCIaudQ9B44VCWLfhLVFC5ykxr6ha73xdVmu'),
+	(12,'sisi','sisi@toto.com','$2a$10$.eyajda.Gx5H45JosxavxeUCcSq0SD3hrVvKV42u89Qq0L/auEnMi');
 
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -133,7 +166,10 @@ VALUES
 	(1,1,8),
 	(2,1,7),
 	(133,7,8),
-	(134,7,7);
+	(135,9,8),
+	(137,11,8),
+	(140,12,8),
+	(141,12,7);
 
 /*!40000 ALTER TABLE `users_interest` ENABLE KEYS */;
 UNLOCK TABLES;
